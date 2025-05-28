0baacd19743135b2abf68644e54471b564ad8b32
EduardoBrito2 <eduardo.brito7@fatec.sp.gov.br>
2025-05-28 20:56:16 -0300

    modified:   README.md 	modified:   escola/.env 	modified:   escola/.gitignore 	modified:   escola/package.json 	modified:   escola/src/controllers/DisciplinaControllers.ts 	modified:   escola/src/controllers/ProfessorController.ts 	modified:   escola/src/controllers/Professor_has_DisciplinaControllers.ts 	modified:   escola/src/index.ts 	modified:   escola/src/models/connection.ts 	modified:   escola/src/models/index.ts 	modified:   escola/src/routes/disciplina.ts 	modified:   escola/src/routes/index.ts

--- escola/src/models/connection.ts
+++ escola/src/models/connection.ts
@@ -1,57 +1,41 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-// A URI do banco de dados MongoDB
+// A URI indica o IP, a porta e BD a ser conectado
 const uri = "mongodb://127.0.0.1:27017/bdaula";
 
 export default function connect() {
-  // Configura manipuladores de eventos para diferentes estados de conexão
-  mongoose.connection.on("connected", () => {
-    console.log("Conectado ao MongoDB");
-  });
+    // Configura manipuladores de eventos para diferentes estados de conexão
+    // cada mensagem de log indica um estado específico da conexão.
+    // É opcional configurar os manipuladores de estado, 
+    // mas é interessante para sabermos sobre a conexão
+    mongoose.connection.on("connected", () => console.log("connected"));
+    mongoose.connection.on("open", () => console.log("open"));
+    mongoose.connection.on("disconnected", () => console.log("disconnected"));
+    mongoose.connection.on("reconnected", () => console.log("reconnected"));
+    mongoose.connection.on("disconnecting", () => console.log("disconnecting"));
+    mongoose.connection.on("close", () => console.log("close"));
+    // Utiliza o método connect do Mongoose para estabelecer a conexão com o MongoDB, usando a URI
+    mongoose
+        .connect(uri, {
+            serverSelectionTimeoutMS: 5000,
+            maxPoolSize: 10,
+        })
+        .then(() => console.log("Conectado ao MongoDB"))
+        .catch((e) => {
+            console.error("Erro ao conectar ao MongoDB:", e.message);
+        });
 
-  mongoose.connection.on("open", () => {
-    console.log("Conexão aberta com o MongoDB");
-  });
-
-  mongoose.connection.on("disconnected", () => {
-    console.log("Desconectado do MongoDB");
-  });
-
-  mongoose.connection.on("reconnected", () => {
-    console.log("Reconectado ao MongoDB");
-  });
-
-  mongoose.connection.on("disconnecting", () => {
-    console.log("Desconectando do MongoDB");
-  });
-
-  mongoose.connection.on("close", () => {
-    console.log("Conexão fechada com o MongoDB");
-  });
-
-  // Utiliza o método connect do Mongoose para estabelecer a conexão com o MongoDB
-  mongoose
-    .connect(uri, {
-      serverSelectionTimeoutMS: 5000, // Timeout de 5 segundos para escolher o servidor
-      maxPoolSize: 10, // Limite de conexões simultâneas ao banco
-    })
-    .then(() => {
-      console.log("Conectado ao MongoDB com sucesso");
-    })
-    .catch((e) => {
-      console.error("Erro ao conectar ao MongoDB:", e.message);
-      process.exit(1); // Encerra a aplicação em caso de erro de conexão
+    // o sinal SIGINT é disparado ao encerrar a aplicação, geralmente, usando Crtl+C
+    process.on("SIGINT", async () => {
+        try {
+            console.log("Conexão com o MongoDB fechada");
+            await mongoose.connection.close();
+            process.exit(0);
+        } catch (error) {
+            console.error("Erro ao fechar a conexão com o MongoDB:", error);
+            process.exit(1);
+        }
     });
-
-  // Lidar com o fechamento do processo
-  process.on("SIGINT", async () => {
-    try {
-      console.log("Fechando a conexão com o MongoDB...");
-      await mongoose.connection.close();
-      process.exit(0); // Finaliza o processo com sucesso
-    } catch (error) {
-      console.error("Erro ao fechar a conexão com o MongoDB:", error);
-      process.exit(1); // Finaliza o processo com erro
-    }
-  });
 }
--- escola/src/routes/index.ts
+++ escola/src/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router, Request, Response } from "express";
-import professor from "./professor";
 import disciplina from "./disciplina";
+import professor from "./professor";
 import professor_has_disciplina from "./professor_has_disciplina";
+
 const routes = Router();
+
 routes.use("/professor", professor);
 routes.use("/disciplina", disciplina);
 routes.use("/professor_has_disciplina", professor_has_disciplina);
+
 //aceita qualquer método HTTP ou URL
 routes.use((_: any, res: any) => res.json({ error: "Requisição desconhecida" }));
+
 export default routes;
